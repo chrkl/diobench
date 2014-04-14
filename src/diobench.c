a054c5f3942f758c83f427719cf369a71d4c932d
Christoph Kleineweber <kleineweber@zib.de>
2014-04-14 18:07:04 +0200

    Added support for multiple repititions

--- src/diobench.c
+++ src/diobench.c
@@ -16,14 +16,14 @@ struct arg_str *block_size;
 struct arg_lit *help;
 struct arg_lit *no_cleanup;
 struct arg_str *work_dir;
+struct arg_int *repititions;
 struct arg_end *end;
 
 int main(int argc, char **argv)
 {
     char *path  = malloc(PATH_LEN * sizeof(char));
     char *dir = malloc(PATH_LEN * sizeof(char));
-    int myrank, ntasks, nerrors, cleanup;
-    bench_result_t result;
+    int myrank, ntasks, nerrors, cleanup, rep, i;
     double total_tp;
 
     void *argtable[] = {
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
         help = arg_lit0("h", "help", "Show help text"),
         no_cleanup = arg_lit0(NULL, "no-cleanup", "Do not clean up benchmark files"),
         work_dir = arg_str0("w", "work-dir", "<dir>", "Work directory"),
+        repititions = arg_int0("r", "repititions", "<repititions>", "Number of repititions"),
         end = arg_end(5)
     };
 
@@ -94,6 +95,15 @@ int main(int argc, char **argv)
     	getcwd(dir, PATH_LEN);
     }
 
+    if(repititions->count > 0)
+    {
+        rep = repititions->ival[0];
+    }
+    else
+    {
+        rep = 1;
+    }
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
@@ -106,42 +116,53 @@ int main(int argc, char **argv)
 
     if(own_dir->count > 0)
     {
-    	char *own_dir_str;
-    	asprintf(&own_dir_str, "/%d", myrank);
-    	strncat(dir, own_dir_str, PATH_LEN);
-    	mkdir(dir, 0700);
+        char *own_dir_str;
+        asprintf(&own_dir_str, "/%d", myrank);
+        strncat(dir, own_dir_str, PATH_LEN);
+        mkdir(dir, 0700);
     }
 
     snprintf(path, PATH_LEN, "%s/diobench_%d.out", dir, myrank);
-    result = benchmark(path, bs, count, cleanup);
 
-    printf("Rank %d: written %lu MB in %f seconds (%f MB/s)\n",
-           myrank, result.written_bytes / (1024 * 1024),  
-           result.runtime, result.throughput);
-    
-    if(result.status != 0 )
+    for(i = 0; i < rep; i++)
     {
-        printf("Unable to delete the file\n");
-        perror("Error");
-    }
-    
-    if(own_dir->count > 0 && cleanup)
-    {
-    	rmdir(dir);
+        bench_result_t result;
+
+        if(rep > 1 && myrank == 0)
+        {
+            printf("Starting run number %d\n", i+1);
+        }
+
+        result = benchmark(path, bs, count, cleanup);
+
+        printf("Rank %d: written %lu MB in %f seconds (%f MB/s)\n",
+               myrank, result.written_bytes / (1024 * 1024),  
+               result.runtime, result.throughput);
+        
+        if(result.status != 0 )
+        {
+            printf("Unable to delete the file\n");
+            perror("Error");
+        }
+        
+        if(own_dir->count > 0 && cleanup)
+        {
+        	rmdir(dir);
+        }
+
+        arg_free(argtable);
+
+        if(ntasks > 1)
+        	MPI_Reduce(&result.throughput, &total_tp, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    	else
+    		total_tp = result.throughput;
+
+        if(myrank == 0)
+            printf("Total throughput: %f MB/s\n", total_tp);
     }
 
     free(dir);
     free(path);
-    arg_free(argtable);
-
-    if(ntasks > 1)
-    	MPI_Reduce(&result.throughput, &total_tp, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	else
-		total_tp = result.throughput;
-
-    if(myrank == 0)
-        printf("Total throughput: %f MB/s\n", total_tp);
-
     MPI_Finalize();
     return 0;
 }
